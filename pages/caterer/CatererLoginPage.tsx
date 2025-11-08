7c9426c72a84eb8fd453765f7bfcda34d555feb8
Muhammad Waleed <132374409+iMuhammadWaleed@users.noreply.github.com>
2025-11-08 20:45:30 +0500

    fix(login): Remove commented-out code
    Remove a commented-out line from CatererLoginPage.tsx related to completing the input element. This code was likely a leftover from development and is no longer needed.

--- pages/caterer/CatererLoginPage.tsx
+++ pages/caterer/CatererLoginPage.tsx
@@ -83,7 +83,6 @@ const CatererLoginPage: React.FC = () => {
                                             <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                                         </svg>
                                     </span>
-                                    {/* FIX: Complete the input element and add the rest of the component logic. */}
                                     <input
                                         id="password"
                                         name="password"
